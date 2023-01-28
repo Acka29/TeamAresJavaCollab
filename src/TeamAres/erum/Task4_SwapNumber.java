9f2e24ae1fca2937d2f6093c899094d644163b8f
Erum_Kaleemullah <erumkashif@hotmail.com>
2023-01-27 19:59:07 -0600

    Task 4 updated

--- src/TeamAres/erum/Task4_SwapNumber.java
+++ src/TeamAres/erum/Task4_SwapNumber.java
@@ -2,9 +2,7 @@ package TeamAres.erum;
 
 public class Task4_SwapNumber {
     public static void main(String[] args) {
-        int a=-10;
-
-        System.out.println(a);
+        int a=10;
         int b=20;
         a=a+b;//10+20=30
         b=a-b; //30-20=10
