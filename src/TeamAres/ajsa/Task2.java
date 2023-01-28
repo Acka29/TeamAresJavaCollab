4beab7334b172950848ba8c17e256773ca03834d
AjsaHusam <ajsadz@gmail.com>
2023-01-27 20:09:23 -0600

    Task2 completed

---
+++ src/TeamAres/ajsa/Task2.java
@@ -0,0 +1,27 @@
+package TeamAres.ajsa;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        divide(10, 3);
+        divide(20, 3);
+    }
+
+    public static void divide(int num1, int num2) {
+        if (num2 == 0) {
+            System.out.println("Invalid Number");
+            return;
+        }
+
+        int count = 0;
+        while (num1 >= num2) {
+            num1 -= num2;
+            count++;
+        }
+        System.out.println(count + " and remainder is " + num1);
+    }
+
+
+}
+
+
