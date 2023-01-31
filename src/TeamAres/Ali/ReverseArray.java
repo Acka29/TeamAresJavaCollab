4d2f84e9e883ff53d849427d4ed02ad22e742528
alisabanci <alisabanci@hotmail.com>
2023-01-30 21:15:27 -0600

    Reverse Array Task

---
+++ src/TeamAres/Ali/ReverseArray.java
@@ -0,0 +1,21 @@
+package TeamAres.Ali;
+
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        int [] arr = {1,2,3,4,5,6,7};
+        int[] reverse = new int[arr.length];
+
+        for(int i = arr.length-1, j = 0 ; i >= 0 ; i--){
+            reverse[j++] = arr[i];
+
+        }
+
+        System.out.println(Arrays.toString(reverse));
+    }
+
+}
