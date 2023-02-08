be09b48f7e2c5aa4f5eecc49c40bf27530bc630b
alisabanci <alisabanci@hotmail.com>
2023-02-07 22:33:38 -0600

    Finra Task

--- src/TeamAres/Ali/FinRa.java
+++ src/TeamAres/Ali/FinRa.java
@@ -7,7 +7,7 @@ package TeamAres.Ali;
             String result = "";
             for (int i = 1; i < 101; i++) {
 
-                if (i % 15 == 0) {
+                if (i % 3 == 0 && i%5==0) {
                     result += "FINRA ";
                 } else if (i % 5 == 0) {
                     result += "RA ";
